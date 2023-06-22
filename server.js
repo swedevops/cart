decde4a49b830181bb7a728eeb804dcdb1ecba2f
swedev99 <swedevops99@gmail.com>
2023-06-22 14:33:47 +0530

    Jenkins pipeline concepts

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const pino = require('pino');
 const expPino = require('express-pino-logger');
-// Prometheus
+// Prometheusf
 const promClient = require('prom-client');
 const Registry = promClient.Registry;
 const register = new Registry();
